1fe8aea4292d9e5dc7e6d6706e076e40cda24d3b
Elin de wit <Elin.dewit04@gmail.com>
2023-02-04 23:02:06 +0100

    roles

--- database.mjs
+++ database.mjs
@@ -1,4 +1,4 @@
-import { MongoClient, ObjectId } from 'mongodb';
+import { Admin, MongoClient, ObjectId } from 'mongodb';
 
 const uri = "mongodb://127.0.0.1:27017/"
 const a = "assignments"
@@ -14,6 +14,11 @@ export async function connectToDB(uri, collectionName) {
         const db = mongoClient.db('services');
         const collection = db.collection(collectionName);
 
+        const rootuser = await db.collection('users').findOne({ Name: "root" })
+        if (!rootuser) {
+            await db.collection('users').insertOne({ Name: "root", Vorname: "root", password: "1", role: ["admin"]})
+        }
+
         return collection;
     } catch (error) {
         console.error('Connection to MongoDB failed!', error);
@@ -27,7 +32,7 @@ export async function createAssignment(assignment) {
 
 export async function searching(search) {
     const collection = await connectToDB(uri, a);
-    return await collection.find(search&&search.name ? search : undefined).toArray();
+    return await collection.find(search && search.name ? search : undefined).toArray();
 }
 
 export async function getById(id) {
@@ -47,11 +52,20 @@ export async function finduser(user) {
 
 export async function deleteAssignments(ids) {
     const collection = await connectToDB(uri, a);
-    console.log({_id: {$in: ids.map((id) => ObjectId(id))}});
-    console.log( await collection.deleteMany({_id: {$in: ids.map((id) => ObjectId(id))}}));
+    await collection.deleteMany({ _id: { $in: ids.map((id) => ObjectId(id)) } });
 }
 
 export async function updateAssignment(assignment) {
     const collection = await connectToDB(uri, a);
-    await collection.replaceOne({_id: ObjectId(assignment.id)},assignment);
+    await collection.replaceOne({ _id: ObjectId(assignment.id) }, assignment);
+}
+
+export async function searchUser(search) {
+    const collection = await connectToDB(uri, 'users');
+    return await collection.find(search && search.Name ? search : undefined).toArray();
+}
+
+export async function deleteUsers(ids) {
+    const collection = await connectToDB(uri, 'users');
+    await collection.deleteMany({ _id: { $in: ids.map((id) => ObjectId(id)) } });
 }
--- index.mjs
+++ index.mjs
@@ -3,14 +3,21 @@ import session from 'express-session';
 import mustacheExpress from 'mustache-express';
 import hbs from 'hbs';
 import bodyParser from 'body-parser'
-import { createAssignment, createUser, deleteAssignments, finduser, getById, searching,updateAssignment } from './database.mjs'
+import { createAssignment, createUser, deleteAssignments, deleteUsers, finduser, getById, searching, searchUser, updateAssignment } from './database.mjs'
 const app = express()
 const port = 3000
 
-function isAuthenticated (req, res, next) {
-  if (req.session.user && req.session.user.password) next()
-  // else res.redirect('/')
-  next()
+function isAuthenticated(role) {
+  return (req, res, next) => {
+    const user = req.session.user
+    const roles = Array.isArray(user.role) ? user.role : [user.role];
+    if (user.password && roles.some((r) => role.includes(r))) {
+      next()
+    }
+    else {
+      res.redirect('/')
+    }
+  }
 }
 
 app.engine('mustache', mustacheExpress());
@@ -22,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-  cookie: {secure: false}, secret: "keyboard cat", resave: false,
+  cookie: { secure: false }, secret: "keyboard cat", resave: false,
   saveUninitialized: false
 }))
 
@@ -30,24 +37,23 @@ app.use((req, res, next) => {
   if (!req.session.user) {
     req.session.user = {};
   }
-
   next();
 })
 
-app.post('/auftrag',isAuthenticated, (req, res) => {
-  if (req.body.id){
+app.post('/auftrag', isAuthenticated(["admin", "coworker"]), (req, res) => {
+  if (req.body.id) {
     updateAssignment(req.body)
   }
-    else{
+  else {
     createAssignment(req.body);
   }
-  res.render("back", {type: "assignment"})
+  res.render("back", { type: "assignment" })
 })
 
 
-app.post('/user',isAuthenticated, (req, res) => {
+app.post('/user', isAuthenticated(["admin", "manager"]), (req, res) => {
   createUser(req.body);
-  res.render("back", {type: "user"})
+  res.render("back", { type: "user" })
 })
 
 app.post('/login', async (req, res) => {
@@ -58,40 +64,58 @@ app.post('/login', async (req, res) => {
       req.session.user = user
       req.session.save(function (err) {
         if (err) return next(err)
-        res.redirect('/main.html')
+        res.redirect('/static/main.html')
       })
     })
   }
   else res.render('error')
 })
 
-app.get('/print/:id', isAuthenticated, async (req, res) => {
-const displayassignments = await getById(req.params.id)
-res.render("formular", displayassignments)
+app.get('/print/:id', isAuthenticated(["admin", "coworker"]), async (req, res) => {
+  const displayassignments = await getById(req.params.id)
+  res.render("formular", displayassignments)
 })
 
-app.get('/edit/:id?', isAuthenticated, async (req, res) => {
+app.get('/edit/:id?', isAuthenticated(["admin", "manager"]), async (req, res) => {
   const edit = await getById(req.params.id)
   res.render("form", edit)
 })
 
-app.post('/delete', isAuthenticated,async(req, res) => {
-  await deleteAssignments(req.body.id);
+app.post('/delete', isAuthenticated(["admin", "manager"]), async (req, res) => {
+  await deleteAssignments(Array.isArray(req.body.id) ? req.body.id : [req.body.id]);
   const auftrage = await searching(req.body);
   res.render("list", { auftrage, ...req.body });
 })
 
-app.post('/search',isAuthenticated, async (req, res) => {
+app.post('/search', isAuthenticated(["admin", "manager", "coworker"]), async (req, res) => {
   const result = await searching(req.body);
-  console.log(req.body, result);
   res.render("list", { auftrage: result, ...req.body })
 })
 
-app.get("/list",isAuthenticated, async (req, res) => {
+app.get("/list", isAuthenticated(["admin", "manager", "coworker"]), async (req, res) => {
   res.render("list", { auftrage: await searching() })
 })
 
-app.use(express.static("."))
+app.post('/searchuser', isAuthenticated(["admin", "manager"]), async (req, res) => {
+  const result = await searchUser(req.body);
+  res.render("userlist", { users: result, ...req.body })
+})
+
+app.get("/listuser", isAuthenticated(["admin", "manager"]), async (req, res) => {
+  res.render("userlist", { users: await searchUser() })
+})
+
+app.post('/deleteusers', isAuthenticated(["admin", "manager"]), async (req, res) => {
+  await deleteUsers(Array.isArray(req.body.id) ? req.body.id : [req.body.id]);
+  const users = await searchUser(req.body);
+  res.render("userlist", { users, ...req.body });
+})
+
+app.get('/', (req, res) => {
+  res.render("login")
+})
+app.use('/static', isAuthenticated(["coworker", "manager", "admin"]))
+app.use('/static', express.static("."))
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on port ${port}`)
 })
